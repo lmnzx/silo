6227c89ba978d94101750dcb489e2e27f22d7dda
lmnzx <snyxmk@gmail.com>
2024-12-01 17:19:06 +0530

    finally a text editor

--- silo.c
+++ silo.c
@@ -22,7 +22,7 @@
 #define SILO_VERSION "0.0.1"
 #define SILO_TAB_STOP 8
 
-enum editorKey { ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN, DEL_KEY };
+enum editorKey { BACKSPACE = 127, ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN, DEL_KEY };
 
 typedef struct erow {
     int size;
@@ -232,6 +232,25 @@ void editorAppendRow(char *s, size_t len) {
     E.numrows++;
 }
 
+void editorRowInsertChar(erow *row, int at, int c) {
+    if (at < 0 || at > row->size)
+        at = row->size;
+
+    row->chars = realloc(row->chars, row->size + 2);
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+void editorInsertChar(int c) {
+    if (E.cy == E.numrows) {
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
 void editorOpen(char *filename) {
     free(E.filename);
     E.filename = strdup(filename);
@@ -416,10 +435,18 @@ void editorProcessKeypress(void) {
     int c = editorReadKey();
 
     switch (c) {
+    case '\r':
+        break;
+
     case CTRL_KEY('q'):
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
+        break;
+
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
         break;
 
     case ARROW_LEFT:
@@ -427,6 +454,14 @@ void editorProcessKeypress(void) {
     case ARROW_UP:
     case ARROW_RIGHT:
         editorMoveCursor(c);
+        break;
+
+    case CTRL_KEY('l'):
+    case '\x1b':
+        break;
+
+    default:
+        editorInsertChar(c);
         break;
     }
 }
